89f1bf17f773ba242dde27da5e5b8b9f3805aec3
yutingh <yutingh@cw.com.tw>
2021-11-22 01:03:46 +0800

    modify
    更新icon，新增服務項目邏輯，img minify

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,49 @@ $('.hamurger').click(function(){
 	$(this).toggleClass('active');
 	$('.nav-group').slideToggle();
 })
+
+$('.navbar-side ul.lv-1 > li > span').click(function(){
+	$(this).toggleClass('opened');
+	$(this).siblings('.lv-2').slideToggle();
+})
+
+function parse_query_string(query) {
+	let vars = query.split("&");
+	let query_string = {};
+	for (var i = 0; i < vars.length; i++) {
+		var pair = vars[i].split("=");
+		var key = decodeURIComponent(pair[0]);
+		var value = decodeURIComponent(pair[1]);
+		if (typeof query_string[key] === "undefined") {
+			query_string[key] = decodeURIComponent(value);
+		} else if (typeof query_string[key] === "string") {
+			var arr = [query_string[key], decodeURIComponent(value)];
+			query_string[key] = arr;
+		} else {
+			query_string[key].push(decodeURIComponent(value));
+		}
+	}
+	return query_string;
+}
+
+let service_string = window.location.search;
+if ( service_string !== '' ) {
+	service_string = service_string.split('?')[1];
+}
+let sort_index = parse_query_string(service_string);
+
+if ( sort_index.sort == undefined || (sort_index.sort == undefined && sort_index.item == undefined) ) {
+	$('.navbar-display-1-1').show();
+	$('.navbar-side ul.lv-1').children('li').eq(0).children('.lv-2').show();
+	$('.navbar-side ul.lv-1').children('li').eq(0).children('span, a').addClass('active');
+	$('.navbar-side ul.lv-1').children('li').eq(0).children('.lv-2').children('li').eq(0).children('a').addClass('active');
+} else if ( sort_index.item == undefined ) {
+	$(`.navbar-display-${sort_index.sort}`).show();
+	$('.navbar-side ul.lv-1').children('li').eq(sort_index.sort-1).children('.lv-2').show();
+	$('.navbar-side ul.lv-1').children('li').eq(sort_index.sort-1).children('span, a').addClass('active');
+} else {
+	$(`.navbar-display-${sort_index.sort}-${sort_index.item}`).show();
+	$('.navbar-side ul.lv-1').children('li').eq(sort_index.sort-1).children('.lv-2').show();
+	$('.navbar-side ul.lv-1').children('li').eq(sort_index.sort-1).children('span, a').addClass('active');
+	$('.navbar-side ul.lv-1').children('li').eq(sort_index.sort-1).children('.lv-2').children('li').eq(sort_index.item-1).children('a').addClass('active');
+}
